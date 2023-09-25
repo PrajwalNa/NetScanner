51f6aec2ca35f7681969f1980ac87f89d83f75a6
Prajwal Nautiyal <prajwalnautiyal2003@gmail.com>
2023-09-24 23:34:08 -0400

    Got around making the tcpCONN functional, and refactored the code

--- netScan.py
+++ netScan.py
@@ -3,7 +3,7 @@
 ---------------------------------
 Dev: Prajwal Nautiyal
 Date: 12 September 2023
-Version: 0.8
+Version: 0.9 (alpha)
 ---------------------------------
 This is a simple network scanner.
 It scans the network and returns the IP, MAC address and, with relative accuracy, the hardware vendor of the devices connected to the network.
@@ -11,223 +11,52 @@ It uses the ARP protocol to scan the network.
 It uses the scapy library to send and receive packets.
 It also has basic port scanning functionality.
 ---------------------------------
-ANSI escape codes:
-    Red Background: \033[48;5;196m
-    Green Background: \033[48;5;82m
-    White Background: \033[48;5;255m
-    Red: \033[38;5;196m
-    Cyan: \033[38;5;87m
-    Green: \033[38;5;82m
-    Yellow: \033[38;5;228m
-    Black: \033[38;5;0m
-    Clear: \033[0m
----------------------------------
-Error Codes:
-    0: No error
-    1: No target IP
-    2: Invalid Port Scan
-    3: Invalid Port Number
-    TODO: Add error codes
----------------------------------
 """
 
-# Importing libraries
-import argparse
-import os
-import sys
-import time
-import threading
-
-import scapy.all as scapy
-
-# Clearing the screen
-os.system("clear")
-
-def getArgs():
-    """
-    Function to get the arguments from the command line
-
-    Returns:
-        options: The arguments
-            options.target: The target IP / IP range [Required]
-            options.port: The port to scan [Optional] !Not yet implemented!
-            options.default: Scan the default ports [Optional] !Not yet implemented!
-            options.scanType: The type of scan [Optional] !Not yet implemented!
-    
-    Raises:
-        error code 1: If no target IP is specified
-        error code 2: If both the port and the default port scan are specified
-        error code 3: If the port number is invalid
-    """
-    parser = argparse.ArgumentParser()                  # Creating the parser object
-    # Adding the arguments
-    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP Range.", required=True)
-    parser.add_argument("-p", "--port", dest="port", help="Port to scan.", required=False)
-    parser.add_argument("-d", "--default", dest="default", help="Scan the default ports.", required=False, action="store_true")
-    parser.add_argument("-s", "--scan-type", dest="scanType", help="Scan type.", required=False, choices=["tcpCONN","tcpSYN", "udp"])
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.8")
-    options = parser.parse_args()                       # Parsing the arguments
-    if not options.target:                              # Checking if the target IP / IP range is specified, exiting if not
-        parser.error("\033[38;5;196m[-] Please specify a target IP/IP range\n\033[38;5;228m[*]Use --help for more info.\033[0m")
-        sys.exit(1)
-    if options.default and options.port:                # Checking if both the port and the default port scan are specified, exiting if so
-        parser.error("\033[38;5;196m[-] Please specify either a port or the default port scan.\033[0m")
-        sys.exit(2)
-    if options.port:
-        try:
-            options.port = int(options.port)            # Checking if the port number is valid integer, exiting if not
-        except ValueError:
-            parser.error("\033[38;5;196m[-] Please specify a valid port number.\033[0m")
-            sys.exit(3)
-        if options.port < 1 or options.port > 65535:    # Checking if the port number is valid, exiting if not
-            parser.error("\033[38;5;196m[-] Please specify a valid port number.\033[0m")
-            sys.exit(3)
-        if options.scanType == None:                    # Setting the scan type to tcpCONN if not specified
-            options.scanType = "tcpCONN"
-    return options
-
-
-class subnetScan(threading.Thread):
-    """
-    Class to scan the network
-
-    Args:
-        ip (str): The IP range to scan
-
-    Returns:
-        None
-
-    Methods:
-        run: Function to scan the network, and print the results
-        getVendor: Function to get the vendor of a device with the specified MAC address using macvendors API
-        displayScan: Function to display the scan results
-
-    Attributes:
-        answeredList: The list of answered packets
-        ipRange: The IP range to scan
-        mac: The MAC address of the device
-        vendor: The vendor of the device
-        resultsDict: The dictionary containing the results
-
-    Raises:
-        None
-    """
-    def __init__(self, ip):
-        threading.Thread.__init__(self)
-        # Checking if the IP range is specified in CIDR notation, if not, adding it
-        if ip.find("/") == -1:
-            pos = ip.rfind(".")                                                 # Finding the last occurence of a dot    
-            ip = ip[:pos] + ".1/24"                                             # Slicing the IP from the postion of last '.' and adding the CIDR notation
-        self.answeredList = []
-        self.ipRange = ip
-        self.mac = ""
-        self.vendor = ""
-        self.resultsDict = {}
-        
-    def run(self):
-        """
-        Function to scan the network, and print the results
-
-        Args:
-            ip (str): The IP range to scan
-
-        Returns:
-            None
-
-        Raises:
-            None
-        """
-        print("\033[38;5;228m[+] Press Ctrl+C to stop the scan.\033[0m")
-        arpRequest = scapy.ARP(pdst=self.ipRange)                                       # Creating the ARP segment
-        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")                                # Creating the ethernet frame
-        packet = broadcast/arpRequest                                                   # Combining the ethernet frame and the ARP segment
-        while self.answeredList == []:
-            self.answeredList = scapy.srp(packet, timeout=1, verbose=False)[0]          # Sending the packet until receiving the response
-        self.displayScan()                                                              # Displaying the scan results
-    
-    def getVendor(self, mac):
-        """
-        Function to get the vendor of a device with the specified MAC address using macvendors API
-
-        Args:
-            mac (str): The MAC address of the device
-
-        Returns:
-            None
-        """
-        self.mac = mac.upper()                                                              # Converting the MAC address to uppercase
-        self.mac = self.mac.replace(":", "-")                                               # Replacing the colons with hyphens
-        self.vendor = os.popen(f"curl -s https://api.macvendors.com/{self.mac}").read()     # Getting the vendor from the API
-        if str(self.vendor).find("errors") != -1:                                           # Checking if the vendor is not found
-            self.vendor = "Unknown"
-
-    def displayScan(self):
-        """
-        Function to display the scan results
-
-        Args:
-            answeredList: The list of answered packets
-
-        Returns:
-            None
-        """
-        for element in self.answeredList:
-            self.getVendor(element[1].hwsrc)
-            self.resultsDict[element[1].psrc] = [element[1].hwsrc, self.vendor]
-        print("\r" + " " * 100, end="", flush=True)
-        print("\r\033[38;5;82m[+] Network Scan complete.\033[0m")
-        print(f"\n\033[48;5;255;38;5;0m|{'IP':<20}|{'MAC Address':<20}|{'Vendor':<68}|\033[0m")
-        print(f"\033[38;5;228m|{'-'*20}|{'-'*20}|{'-'*68}|\033[0m")
-        for key in self.resultsDict:
-            print(f"\033[38;5;228m|\033[38;5;87m{key:<20}\033[38;5;228m|\033[38;5;87m{self.resultsDict[key][0]:<20}\033[38;5;228m|\033[38;5;87m{self.resultsDict[key][1]:<68}\033[38;5;228m|\033[0m")
-        print(f"\033[38;5;228m|{'-'*20}|{'-'*20}|{'-'*68}|\033[0m")
-        print(f"\033[38;5;82m[+] Total devices found: {len(self.resultsDict)}\033[0m")
-        print("\n")
-
-
-class portScan(threading.Thread):
-    def __init__(self, ip, port):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.resultsDict = {}
-
-def scanPort(ip, port):
-    print("\033[38;5;196m[-] Port scanning is not yet implemented.\033[0m")
-
-def scanDefaultPorts(ip):
-    print("\033[38;5;196m[-] Default port scanning is not yet implemented.\033[0m")
+from netCode import *  
 
 def main():
     """
     Function to run the program
     Exits when CTRL + C is pressed
     """
-    options = getArgs()                                 # Getting the arguments
-    if options.default:                                 # Checking if the default port scan is specified
-        scanDefaultPorts(options.target)    
-    elif options.port:                                  # Checking if the port is specified
-        scanPort(options.target, options.port)
-    else:                                               # If neither the port nor the default port scan is specified a network scan is performed
-        try:
-            while True:
-                scan = subnetScan(options.target)
-                print("\033[38;5;82m[+] Scanning the network.\033[0m")
-                scan.start()
-                while scan.is_alive():
-                    for i in ['.    ', '..   ', '...  ', '.... ', '.....', ' ....', '  ...', '   ..', '    .', '     ']:
-                        print(f"\r\033[38;5;82m[+] Scanning the network {i}", flush=True, end="")
-                        time.sleep(0.12)
-                # Prompting the user to update the results
-                print("\r" + " " * 100, end="", flush=True)
-                update = input("\r\n\033[38;5;228m[+] Do you want to update the results? (y/n): \033[0m").lower()
-                if update != "y":                       # Checking if the user wants to update the results
-                    sys.exit(0)                         # Exiting if not
-                os.system("clear")
-                print("\033[38;5;82m[+] Updating the results.\033[0m")    
-        except KeyboardInterrupt:
-            print("\n\033[38;5;228m[-] Keyboard Interrupt. Exiting...\033[0m")
-            sys.exit(0)
+    if not isConnected():
+        print("\033[38;5;196m[-] Error: No network connection found!!\033[0m")
+        sys.exit(3)
+    options = getArgs()                             # Getting the arguments
+    os.system("clear")
+    try:
+        routing(options)
+    except KeyboardInterrupt:
+        print("\n\033[38;5;228m[-] Keyboard Interrupt. Exiting...\033[0m")
+        sys.exit(0)
+
+def routing(op):
+    """
+    Function to route the program
+
+    Parameters:
+        op (object): The object containing the arguments
+    """
+    while True:
+        scan = subnetScan(op.target)
+        # setting the scan thread as a daemon thread so that it exits when the main thread exits
+        scan.daemon = True
+        if not op.net:
+            scan.start()
+            loadAni(scan)
+            scan.join()
+        if not op.port == None:
+            pScan = portScan(op.target, op.port, op.scanType, op.verbosity, op.all)
+            # same thing as scan thread
+            pScan.daemon = True
+            # if the user did not deny the subnet scan
+            if not op.net:
+                pScan.setIP(scan.getIPs())
+            pScan.start()
+            loadAni(pScan)
+            pScan.join()
+        updateRes()
 
 if __name__ == "__main__":
     main()
